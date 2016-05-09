db1bc9fe92c84d52f633d50949000cd36351b420
yennefer <mnic04032012@gmail.com>
2016-05-09 16:45:21 -0700

    first commit

---
+++ device/lge/hammerhead/apps/CheckService/src/com/sentinel/android/checkservice/test.java
@@ -0,0 +1,12 @@
+import android.app.Application;
+import android.os.ServiceManager;
+import android.util.Log;
+import com.sentinel.android.services.check.ICheckService;
+
+public class test {
+ public static void main(String [ ] args)
+{
+     System.out.println("test");
+}
+
+}
